403555ee2dd57d6d8125f1c294f70af9fa54bace
MagdielPm <magdiel_joab@hotmail.com>
2022-05-13 16:32:45 -0500

    feat: remove access token to get all books

--- src/routes/book.routes.js
+++ src/routes/book.routes.js
@@ -1,11 +1,7 @@
 import { Router } from "express";
 import { getAllBooks } from "../controllers/books.controller";
 
-import { verifyMyActiveToken } from "../middleware/middleware";
-
 const router = Router();
-
-router.use(verifyMyActiveToken);
 
 router.get("/", getAllBooks);
 
