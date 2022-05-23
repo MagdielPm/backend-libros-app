906ea942ac773c731bb154f79727fdfbbc179da9
MagdielPm <magdiel_joab@hotmail.com>
2022-05-23 17:46:26 -0500

    fix issue update user

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -174,6 +174,10 @@ export async function userLogin(req, res) {
         token: createToken(user),
       });
     }
+    return res.status(201).json({
+      message: "User updated successfully",
+      data: user,
+    });
   } catch (error) {
     console.log(error);
 
